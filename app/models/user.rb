8222ab6ef716bcab3a6a955759f09ea19b466055
ErnestsEida <ernestseida14@gmail.com>
2021-11-03 12:17:20 +0200

    Fixing what was deleted while resolving conflicts in PR

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
 
          has_many :posts
 end
