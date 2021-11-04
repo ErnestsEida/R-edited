ea7ce6f75f29a5fd33a728455a5cc4bf5c4b8358
ErnestsEida <ernestseida14@gmail.com>
2021-11-04 12:29:19 +0200

    Removed unneeded method , and adjusted the tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,19 +1,5 @@
 require 'rails_helper'
 
-# Checks if all errors come from given attribute , returns false if otherwise
-def test_attribute_of_post(obj , attribute)
-  # obj : object to check the attributes from
-  # attribute: attribute name of the given object to check errors on
-  obj.valid?
-  error_count = obj.errors.count
-  attribute_err_count = obj.errors[attribute].count
-  if(attribute_err_count < error_count)
-    return false
-  else
-    return true
-  end
-end
-
 RSpec.describe Post, type: :model do
   let(:post) {FactoryBot.build :post}
 
@@ -29,33 +15,21 @@ RSpec.describe Post, type: :model do
 
     it "with title missing" do
       post.title = nil
-      if(!test_attribute_of_post(post , :title))
-        print "[ERROR]"
-      end
       expect(post).to_not be_valid
     end
 
     it "with title shorter than 3 chars" do
       post.title = "x"
-      if(!test_attribute_of_post(post , :title))
-        print "[ERROR]"
-      end
       expect(post).to_not be_valid
     end
 
     it "with title longer than 70 chars" do
       post.title = "x" * 71
-      if(!test_attribute_of_post(post , :title))
-        print "[ERROR]"
-      end
       expect(post).to_not be_valid
     end
 
     it "with description missing" do
       post.description = nil
-      if(!test_attribute_of_post(post , :description))
-        print "[ERROR]"
-      end
       expect(post).to_not be_valid
     end
 
