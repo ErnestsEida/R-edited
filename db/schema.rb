ea8439ceb126b90ee95b2699fb45189a4cab3b60
ErnestsEida <ernestseida14@gmail.com>
2021-11-05 09:43:47 +0200

    Changed posts column from description to content. Adjusted other files where description was mentioned

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_093612) do
+ActiveRecord::Schema.define(version: 2021_11_05_073121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "description"
+    t.text "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 2021_11_03_093612) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
