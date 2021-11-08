4c6b5b277c853e42ed81b8613553dcb95779ba5b
ErnestsEida <ernestseida14@gmail.com>
2021-11-08 14:18:14 +0200

    Issue with destroy routing fixed , by adding path session to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :users, only:[:edit, :update]
-  delete 'users/destroy/:id', to:'users#destroy'
-  devise_for :users
+  resources :users, only:[:edit, :update , :destroy]
+  devise_for :users , path: 'session'
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
