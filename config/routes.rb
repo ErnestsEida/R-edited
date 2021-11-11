e54226ee98574757c6fb09a9dbb5063bb4ff58fb
ErnestsEida <ernestseida14@gmail.com>
2021-11-11 16:37:33 +0200

    Fixing style issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :users , path: 'session'
-  resources :users, only:[:edit, :update , :destroy]
+  resources :users, only: [:edit, :update , :destroy]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
