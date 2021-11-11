71e17be0a1894a19acda70c3cc2f78acc1374af1
ErnestsEida <ernestseida14@gmail.com>
2021-11-11 14:28:52 +0200

    Added Posts controller , {edit , new , show} views , routes. Views formatted with bootstrap

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,57 @@
+class PostsController < ApplicationController
+  before_action :require_post
+  skip_before_action :require_post, only: [:new, :create]
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    if Post.create(post_params)
+      flash[:notice] = "Post successfully created!"
+      redirect_to new_post_path
+    else
+      flash.now[:alert] = "Error occured while making post!"
+      render :new
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      flash.now[:notice] = "Post successfully updated!"
+      render :edit
+    else
+      flash.now[:alert] = "Error occured while updating post!"
+      render :edit
+    end
+  end
+
+  def edit
+  end
+
+  def show
+    if user_signed_in?
+      if User.find(@post.user_id) == current_user
+        @owner = true
+      else
+        @owner = false
+      end
+    else
+      @owner = false
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content, :user_id)
+  end
+
+  def require_post
+    @post = Post.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :users, only:[:edit, :update , :destroy]
+  resources :posts
   devise_for :users , path: 'session'
 
   if Rails.env.development?
