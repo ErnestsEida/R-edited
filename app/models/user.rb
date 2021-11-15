5cbe92c1c2a854120f0e3790bd4ab6b088635ab6
ErnestsEida <ernestseida14@gmail.com>
2021-11-15 10:47:12 +0200

    Implemented username as means to loggin in, besides email

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,19 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true, length: { minimum: 4 , maximum: 16 }
 
   has_many :posts
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
