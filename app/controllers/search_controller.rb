776c52992e4967e9538f05b991c1e6ea6922d9cf
ErnestsEida <ernestseida14@gmail.com>
2021-12-02 13:56:31 +0200

    Search bar implemented

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,11 @@
+class SearchController < ApplicationController
+  def search_query
+    @query = params[:query]
+    @users = @posts = @communities = []
+    if @query != ""
+      @users = User.where("lower(username) LIKE ?", "%#{@query.downcase}%")
+      @posts = Post.where("lower(title) LIKE ?", "%#{@query.downcase}%")
+      @communities = Community.where("lower(title) LIKE ?", "%#{@query.downcase}%")
+    end
+  end
+end
