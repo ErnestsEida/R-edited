0d4193a8e94887e8a24b3b3ba1703ed27c53e0b5
ErnestsEida <ernestseida14@gmail.com>
2021-12-07 10:25:39 +0200

    Removed unnecessary logic from tags controller and views

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,5 @@
 class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
-    @communities = Community.joins(:tag_references).where(tag_references: { tag_id: @tag.id })
-    @posts = Post.joins(:tag_references).where(tag_references: { tag_id: @tag.id })
   end
 end
