cc6df824eeb5636f612b0e127394c245d639516f
ErnestsEida <ernestseida14@gmail.com>
2021-12-08 22:28:43 +0200

    Tagable concern code fix

--- app/models/concerns/tagable.rb
+++ app/models/concerns/tagable.rb
@@ -2,11 +2,12 @@ module Tagable
   extend ActiveSupport::Concern
 
   def tags=(json)
-    return nil unless json.present?
-      tags_params = JSON.parse(json)
-      tags_params.each do |tag_param|
-        tag = Tag.find_or_create_by(title: tag_param['value'])
-        self.tag_references.find_or_initialize_by(tag: tag)
+    return if json.blank?
+
+    tags_params = JSON.parse(json)
+    tags_params.each do |tag_param|
+      tag = Tag.find_or_create_by(title: tag_param['value'])
+      self.tag_references.find_or_initialize_by(tag: tag)
     end
   end
 
