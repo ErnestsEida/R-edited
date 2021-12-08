b5bdef25ad5c67490e550b9d2b359a83ad2ded9e
ErnestsEida <ernestseida14@gmail.com>
2021-12-08 09:55:01 +0200

    Pagination added to tags view

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,6 @@
 class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
+    @posts = Kaminari.paginate_array(@tag.posts.all).page(params[:page]).per(3)
   end
 end
