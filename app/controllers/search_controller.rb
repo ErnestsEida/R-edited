9b0e099e7c83b0e73a36f816a1514b89eb231a94
ErnestsEida <ernestseida14@gmail.com>
2021-12-09 10:13:26 +0200

    Search incorrect controller action name fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchController < ApplicationController
-  def show
+  def search_query
     @query = params[:query]
     @users = @posts = @communities = []
-    if @query.blank?
+    if @query.present?
       @users = User.where("lower(username) LIKE ?", "%#{@query.downcase}%")
       @posts = Post.where("lower(title) LIKE ?", "%#{@query.downcase}%")
       @communities = Community.where("lower(title) LIKE ?", "%#{@query.downcase}%")
