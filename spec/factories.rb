ef4922c88d4a22a724f87f3cd35141b59fc2f1b6
Ingus Skaistkalns <ingus.skaistkalns@makit.lv>
2021-12-17 15:24:46 +0200

    Update factories and fix posts spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,18 @@
 # Post model factory
 FactoryBot.define do
   factory :post do
-    title {"Title"}
-    content {"Description"}
+    title { "Title" }
+    content { "Description" }
     user
+    community
   end
 
   factory :user do
-    email {"example@example.com"}
+    email { "example@example.com" }
+  end
+
+  factory :community do
+    title { Faker::Game.title }
+    user
   end
 end
