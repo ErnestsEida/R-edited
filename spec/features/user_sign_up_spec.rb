ca27d5f71be7479aa5ec52e869d464e6a7593a95
Ingus Skaistkalns <ingus.skaistkalns@makit.lv>
2021-12-17 16:27:27 +0200

    Add capybara spec

---
+++ spec/features/user_sign_up_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'User sign up' do
+  it 'is not successful because user does not provide equal passwords' do
+    visit '/'
+
+    within 'nav.navbar' do
+      click_on "Sign Up"
+    end
+
+    within '.card.auth-card' do
+      fill_in "Email", with: 'draknight@example.com'
+      fill_in "Username", with: 'draknight'
+      fill_in 'Password', with: "password123"
+      fill_in 'Password confirmation', with: "password456"
+      click_button "Sign up"
+    end
+
+    within '.card.auth-card' do
+      expect(page).to have_content("Password confirmation doesn't match Password")
+    end
+
+    expect(User.where(email: 'draknight@example.com')).to_not be_exist
+  end
+end
