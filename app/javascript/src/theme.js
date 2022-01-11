4db07395990ba41fe652609711793be9a047f2b4
ErnestsEida <ernestseida14@gmail.com>
2022-01-11 12:59:43 +0200

    js-cookie added, fixed issues with code

--- app/javascript/src/theme.js
+++ app/javascript/src/theme.js
@@ -1,55 +1,35 @@
+import Cookies from 'js-cookie';
+
 const cookieName = "dark-theme";
 const darkTheme = 1;
 const lightTheme = 0;
 
 document.addEventListener("turbolinks:load", () => {
-  let theme = getCookie();
+  let theme = Cookies.get(cookieName);
 
-  if (theme === null) {
-    setCookie(darkTheme);
+  if (theme === undefined) {
+    Cookies.set(cookieName, darkTheme);
   } else if (theme == darkTheme) {
     setTheme(darkTheme);
-    $("#themeSwitch").click();
+    $("#js-theme-switch").click();
   } else {
     setTheme(lightTheme);
   }
 
-  $("#themeSwitch").click(function() {
+  $("#js-theme-switch").change(function() {
     if ($(this).prop("checked")) { // Dark mode
-      setCookie(darkTheme);
+      Cookies.set(cookieName, darkTheme);
       setTheme(darkTheme);
     } else { // Light mode
-      setCookie(lightTheme);
+      Cookies.set(cookieName, lightTheme);
       setTheme(lightTheme);
     }
   });
 })
 
-function getCookie() {
-  let allCookies = document.cookie;
-  let splitCookies = allCookies.split(";");
-
-  for (let cookie of splitCookies){
-    if (cookie.includes(cookieName)) {
-      let value = cookie.split("=")[1];
-      return value;
-    }
-  }
-
-  return null;
-}
-
-function setCookie(state) {
-  let expDate = new Date();
-  let year = expDate.getFullYear();
-  expDate.setFullYear(year + (2037 - year));
-  let newCookieString = cookieName + "=" + state + ";" + "expires=" + expDate.toUTCString();
-  document.cookie = newCookieString;
-}
-
 function setTheme(themeNum) {
-  $("body").removeClass();
-  
+  $("body").removeClass("light-theme");
+
   if (themeNum === lightTheme) {
     $("body").addClass("light-theme");
   }
