b45053c0726dcc4b90d178241af9c585008ccdcb
ErnestsEida <ernestseida14@gmail.com>
2022-01-12 09:47:35 +0200

    Light theme javascript rewritted, light theme elements adjusted

--- app/javascript/src/theme.js
+++ app/javascript/src/theme.js
@@ -1,36 +1,19 @@
 import Cookies from 'js-cookie';
 
-const cookieName = "dark-theme";
-const darkTheme = 1;
-const lightTheme = 0;
+const cookieName = "light-theme";
 
 document.addEventListener("turbolinks:load", () => {
-  let theme = Cookies.get(cookieName);
+  const isLightThemeOn = Cookies.get(cookieName) === 'true';
+  const $switch = $("#js-theme-switch");
 
-  if (theme === undefined) {
-    Cookies.set(cookieName, darkTheme);
-  } else if (theme == darkTheme) {
-    setTheme(darkTheme);
-    $("#js-theme-switch").click();
-  } else {
-    setTheme(lightTheme);
-  }
+  $switch.change(function() {
+    let isOn = $(this).prop("checked")
 
-  $("#js-theme-switch").change(function() {
-    if ($(this).prop("checked")) { // Dark mode
-      Cookies.set(cookieName, darkTheme);
-      setTheme(darkTheme);
-    } else { // Light mode
-      Cookies.set(cookieName, lightTheme);
-      setTheme(lightTheme);
-    }
+    $("body").toggleClass("light-theme", isOn);
+    Cookies.set(cookieName, isOn, { expires: 365 });
   });
+
+  $switch
+    .prop("checked", isLightThemeOn)
+    .trigger("change");
 })
-
-function setTheme(themeNum) {
-  $("body").removeClass("light-theme");
-
-  if (themeNum === lightTheme) {
-    $("body").addClass("light-theme");
-  }
-}
