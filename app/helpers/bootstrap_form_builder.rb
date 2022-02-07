08217778ed705fed6fe437567d740ec8d8effe4b
ErnestsEida <ernestseida14@gmail.com>
2022-02-07 16:18:42 +0200

    Updated BootstrapFormBuilder and once again used in news post form

--- app/helpers/bootstrap_form_builder.rb
+++ app/helpers/bootstrap_form_builder.rb
@@ -1,25 +1,21 @@
 class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
-  def text_field(attribute, options = {})
-    options[:class] = options[:class].present? ? "#{options[:class]} form-control" : 'form-control'
-    unless @object[attribute].nil?
-      options[:class] += (@object.errors[attribute].any? ? ' is-invalid' : ' is-valid')
-    end
-    super(attribute, options)
-  end
+  %w[text_field text_area file_field].each do |method_name|
+    define_method(method_name) do |attribute, options = {}|
+      feedback_name = @object_name + '-' + attribute.to_s + '-feedback'
+      options["aria-describedby"] = feedback_name
+      options[:class] = options[:class].present? ? "#{options[:class]} form-control" : 'form-control'
+      unless @object[attribute].nil?
+        options[:class] += (@object.errors[attribute].any? ? ' is-invalid' : ' is-valid')
+      end
 
-  def text_area(attribute, options = {})
-    options[:class] = options[:class].present? ? "#{options[:class]} form-control" : 'form-control'
-    unless @object[attribute].nil?
-      options[:class] += (@object.errors[attribute].any? ? ' is-invalid' : ' is-valid')
+      @template.content_tag :div, class: 'form-group' do
+        label(attribute, class: 'form-label') + super(attribute, options) +
+          @template.content_tag(:div, class: 'invalid-feedback', id: feedback_name) do
+            @template.content_tag(:ul) do
+              @template.content_tag(:li, @object.errors[attribute].join(' and '))
+            end
+          end
+      end
     end
-    super(attribute, options)
-  end
-
-  def file_field(attribute, options = {})
-    options[:class] = options[:class].present? ? "#{options[:class]} form-control" : 'form-control'
-    unless @object[attribute].nil?
-      options[:class] += (@object.errors[attribute].any? ? ' is-invalid' : ' is-valid')
-    end
-    super(attribute, options)
   end
 end
