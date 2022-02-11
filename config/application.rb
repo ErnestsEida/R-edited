86ce02d1fcaafb3fcefee2f86e06a72c923c006a
ErnestsEida <ernestseida14@gmail.com>
2022-02-11 15:07:19 +0200

    Add translations to website

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module REdited
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.i18n.available_locales = [:en, :lv]
+    config.i18n.default_locale = :en
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
